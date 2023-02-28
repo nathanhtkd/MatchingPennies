1b7e8386b61e82574fb4c148a3a9ff906bc4f48b
nathanhtkd <nathanhartojo@gmail.com>
2023-02-28 02:08:49 -0600

    completed matching pennies

--- contracts/MatchingPennies.sol
+++ contracts/MatchingPennies.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract MatchingPennies {
     // address variables to keep track of player's address
     address public player1;
@@ -7,14 +9,13 @@ contract MatchingPennies {
     // variable to store the reward for the winner
     uint256 public reward;
     // variables to store player's choice
-    uint256 public player1Choice;
-    uint256 public player2Choice;
-
+    string public player1Choice;
+    string public player2Choice;
 
     function joinGame() public payable {
         require(player1 == address(0x0) || player2 == address(0x0), "Game is full");
         require(msg.sender != player1, "You can't play against yourself!");
-        // msg.value represents amount of ether sent with a message 
+        // msg.value represents amount of ether sent with a message
         require(msg.value >= 0.1 ether, "Minimum bet is 0.1 ETH");
 
         if (player1 == address(0x0)) {
@@ -28,13 +29,17 @@ contract MatchingPennies {
         }
     }
 
-    function play(uint256 choice) public {
-        // only valid players can play 
+    function play(string memory choice) public {
+        // only valid players can play
         require(msg.sender == player1 || msg.sender == player2, "You are not one of the current players!");
         // check if player address have been filled
         require(player1 != address(0x0) && player2 != address(0x0), "There is a missing player slot.");
         // choice has to be 1 or 0
-        require(choice == 0 || choice == 1, "Invalid Choice (0 or 1)");
+        require(
+            keccak256(abi.encodePacked(choice)) == keccak256(abi.encodePacked("a")) ||
+            keccak256(abi.encodePacked(choice)) == keccak256(abi.encodePacked("b")),
+            "Invalid Choice (a or b)"
+        );
 
         if (msg.sender == player1) {
             player1Choice = choice;
@@ -42,27 +47,25 @@ contract MatchingPennies {
             player2Choice = choice;
         }
 
-
         // p1 wins if choices are the same, otherwise p2 wins
-        if (player1Choice == player2Choice) {
-            payable(player1).transfer(reward);
-        } else {
-            payable(player2).transfer(reward);
+        // MAKE SURE TO CHECK IF BOTH STRINGS HAVE BEEN FILLED
+        if (bytes(player1Choice).length != 0 && bytes(player2Choice).length != 0) {
+            console.log("Player 1's Choice was ", player1Choice);
+            console.log("Player 2's Choice was ", player2Choice);
+            if (keccak256(abi.encodePacked(player1Choice)) == keccak256(abi.encodePacked(player2Choice))) {
+                payable(player1).transfer(reward);
+                console.log("Player 1 won .2 ether!");
+            } else {
+                payable(player2).transfer(reward);
+                console.log("Player 2 won .2 ether!");
+            }
+
+            // Reset game, set states to invalid/default
+            player1Choice = "";
+            player2Choice = "";
+            player1 = address(0x0);
+            player2 = address(0x0);
+            reward = 0;
         }
-
-        // Reset game
-        player1Choice = 0;
-        player2Choice = 0;
-        player1 = address(0x0);
-        player2 = address(0x0);
-        reward = 0;
-    }
-
-    function reset() public {
-        player1Choice = 0;
-        player2Choice = 0;
-        player1 = address(0x0);
-        player2 = address(0x0);
-        reward = 0;
     }
 }
